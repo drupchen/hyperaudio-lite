c7e13cf1f97091697953d6c9756365184e4161d6
drupchen <hhdrupchen@gmail.com>
2024-12-27 10:10:00 +0000

    yeshe drupa file 2

--- parse_transcript.py
+++ parse_transcript.py
@@ -165,6 +165,7 @@ file_paths = [
     Path('components/144 A-Menag Dzo-17.srt'),
     Path('components/144 B-Menag Dzo-18.srt'),
     Path('components/112 A-Dzogchen Lamrin Yeshey Drupa.srt'),
+    Path('components/112 B-Dzogchen Lamrin Yeshey Drupa.srt'),
 ]
 
 transcriptions = []
